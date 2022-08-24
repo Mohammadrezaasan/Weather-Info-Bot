121cec5ec61fec9ae6b25e5ad2ff7452de2cf8ca
Mohammadrezaasan <mohammadrezaasan7@gmail.com>
2022-08-24 10:04:07 -0700

    Update weather_info_config.py

--- weather_info_config.py
+++ weather_info_config.py
@@ -1,1 +1,112 @@
+Token_bot = ("Token")
 
+RapidAPI_Key = ("Token")
+
+"----------------------------------------------------------------------------------------------------------"
+sticker = {
+    "Sunny" : "☀️",
+    "Clear" : "🌑",
+    "Partly cloudy" : "🌤",
+    "Cloudy" : "☁️",
+    "Overcast" : "☁️",
+    "Mist" : "😶‍🌫️",
+    "Patchy rain possible" : "🌦",
+    "Patchy snow possible" : "🌨",
+    "Patchy sleet possible" : "🌨",
+    "Patchy freezing drizzle possible" : "🥶❄️",
+    "Thundery outbreaks possible" : "🌩",
+    "Blowing snow" : "🌨",
+    "Blizzard" : "🌬🌨",
+    "Fog" : "😶‍🌫️🌫",
+    "Freezing fog" : "🌫🥶",
+    "Patchy light drizzle" : "🌧",
+    "Light drizzle" : "🌧",
+    "Freezing drizzle" : "🌧❄️",
+    "Heavy freezing drizzle" : "🌧",
+    "Patchy light rain" : "🌦",
+    "Light rain" : "🌧",
+    "Moderate rain at times" : "🌦",
+    "Moderate rain" : "🌧",
+    "Heavy rain at times" : "⛈",
+    "Heavy rain" : "⛈",
+    "Light freezing rain" : "🌧❄️",
+    "Moderate or heavy freezing rain" : "🌧🥶",
+    "Light sleet" : "🌨",
+    "Moderate or heavy sleet" : "🌨",
+    "Patchy light snow" : "🌨",
+    "Light snow" : "🌨",
+    "Patchy moderate snow" : "🌨",
+    "Moderate snow" : "🌨",
+    "Patchy heavy snow" : "🌨🥶",
+    "Heavy snow" : "🌨🥶",
+    "Ice pellets" : "☃️",
+    "Light rain shower" : "🌧",
+    "Moderate or heavy rain shower" : "🌧",
+    "Torrential rain shower" : "⛈🌊",
+    "Light sleet showers" : "🌧",
+    "Moderate or heavy sleet showers" : "⛈",
+    "Light snow showers" : "🌨",
+    "Moderate or heavy snow showers" : "🌨",
+    "Light showers of ice pellets" : "🌨",
+    "Moderate or heavy showers of ice pellets" : "🌨",
+    "Patchy light rain with thunder" : "⛈",
+    "Moderate or heavy rain with thunder" : "⛈",
+    "Patchy light snow with thunder" : "🌨⚡️",
+    "Moderate or heavy snow with thunder" : "🌨⚡️",
+}
+"----------------------------------------------------------------------------------------------------------"
+yes_or_no = {
+    '1' : 'Yes',
+    "0" : 'No'
+}
+"----------------------------------------------------------------------------------------------------------"
+select_day_for_daily_forecast = {
+    "🔮 today's weather forecast 🔮":0,
+    "🔮 tomorrow's weather forecast 🔮":1,
+    "🔮 the weather forecast for the day after tomorrow 🔮":2
+}
+"----------------------------------------------------------------------------------------------------------"
+the_name_of_each_day = {
+    "🔮 today's weather forecast 🔮":'Today',
+    "🔮 tomorrow's weather forecast 🔮":'Tomorrow',
+    "🔮 the weather forecast for the day after tomorrow 🔮":'The Day After Tomorrow'
+}
+"----------------------------------------------------------------------------------------------------------"
+select_day_for_hourly_forecast = {
+    '🕥 hourly forecast today 🕥' : 0,
+    '🕥 hourly forecast for tomorrow 🕥':1,
+    '🕥 hourly forecast the day after tomorrow 🕥' : 2
+}
+"----------------------------------------------------------------------------------------------------------"
+the_name_of_each_day_2 = {
+    '🕥 hourly forecast today 🕥' : 'Today',
+    '🕥 hourly forecast for tomorrow 🕥':'Tomorrow',
+    '🕥 hourly forecast the day after tomorrow 🕥' : 'The Day After Tomorrow'
+}
+"----------------------------------------------------------------------------------------------------------"
+choose_an_hour ={
+    '00:00': 0,    
+    '01:00': 1,    
+    '02:00': 2,    
+    '03:00': 3,    
+    '04:00': 4,    
+    '05:00': 5,    
+    '06:00': 6,    
+    '07:00': 7,    
+    '08:00': 8,    
+    '09:00': 9,    
+    '10:00': 10,    
+    '11:00': 11,    
+    '12:00': 12,    
+    '13:00': 13,    
+    '14:00': 14,    
+    '15:00': 15,    
+    '16:00': 16,    
+    '17:00': 17,    
+    '18:00': 18,    
+    '19:00': 19,    
+    '20:00': 20,    
+    '21:00': 21,    
+    '22:00': 22,    
+    '23:00': 23,    
+}
